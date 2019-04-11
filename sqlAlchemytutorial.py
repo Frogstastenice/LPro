45dc7c63e2e5c341ec50b9aff0efac34ae12106d
Nana Nikolaeva <nana.nikolaeva@gmail.com>
2019-04-11 12:29:30 +0300

    1104

--- sqlAlchemytutorial.py
+++ sqlAlchemytutorial.py
@@ -1,12 +1,16 @@
 import sqlalchemy as db
 
-#engine = db.create_engine('dialect+driver://user:pass@host:port/db')
-
-engine = db.create_engine('mssql+pyodbc://pretender:nAJK9bqo$% 4Gi ^ w34Xko@machineslearn.database.windows.net/TestPythonAnalytics?driver=ODBC+Driver+13+for+SQL+Server)
-#("mssql+pyodbc://scott:tiger@myhost:port/databasename?driver=SQL+Server+Native+Client+10.0")
+engine = db.create_engine('mssql+pyodbc://pretender:nAJK9bqo$%4Gi^w34Xko@machineslearn.database.windows.net/TestPythonAnalytics?driver=ODBC+Driver+13+for+SQL+Server')
 
 connection = engine.connect()
 metadata = db.MetaData()
-census = db.Statuses('census', metadata, autoload=True, autoload_with=engine)
+Statuses = db.Table('Statuses', metadata, autoload=True, autoload_with=engine)
 
-print(census.columns.keys())
+print(Statuses.columns.keys())
+
+
+#Сейчас можно поэкспериментировать. 
+# Подключиться к нужной таблице БД. 
+# Сделать селект, циклом пройти по всем строкам результата. 
+# Для каждой строки сделать объект типа ApplicationStatusHistory, записать в список. 
+# После окончания цикла вывести на экран все объекты из списка.
